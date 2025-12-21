58774d4349e48f7d777367e79110671a881ddd7e
Riley Shaw <rileyjshaw@gmail.com>
2025-12-21 18:22:40 -0500

    Fix favicon config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import favicons from '@peterek/vite-plugin-favicons';
 
+const config = {
+	path: '/big-dipper/',
+	appName: 'Big Dipper',
+	appShortName: 'Big Dipper',
+	appDescription: 'A rhythm machine made with DIP switches.',
+};
+
 export default defineConfig({
-	base: '/big-dipper/',
+	base: config.path,
 	build: {
 		outDir: 'dist',
 	},
-	plugins: [favicons('src/assets/icon.png')],
+	plugins: [favicons('src/assets/icon.png', config)],
 });
